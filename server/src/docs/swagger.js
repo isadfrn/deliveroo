2a5c6a4e1fe642b2275cfebdf72c1d690f28c5e9
isadfrn <isadfrn@gmail.com>
2024-02-21 23:33:14 -0300

    feat: Add missing packages

---
+++ server/src/docs/swagger.js
@@ -0,0 +1,46 @@
+/**
+ * @swagger
+ * components:
+ *   schemas:
+ *     User:
+ *       type: object
+ *       required:
+ *         - username
+ *         - password
+ *       properties:
+ *         id:
+ *           type: integer
+ *           description: The auto-generated id of the user.
+ *         username:
+ *           type: string
+ *           description: The user's username.
+ *         password:
+ *           type: string
+ *           description: The user's password.
+ *       example:
+ *         username: johndoe
+ *         password: Password123
+ */
+
+/**
+ * @swagger
+ * /users:
+ *   post:
+ *     summary: Create a new user
+ *     tags: [Users]
+ *     requestBody:
+ *       required: true
+ *       content:
+ *         application/json:
+ *           schema:
+ *             $ref: '#/components/schemas/User'
+ *     responses:
+ *       200:
+ *         description: The user was successfully created
+ *         content:
+ *           application/json:
+ *             schema:
+ *               $ref: '#/components/schemas/User'
+ *       500:
+ *         description: Some server error
+ */
